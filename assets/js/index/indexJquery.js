f4ec0211f094b8103ded381db1c17573cf3d1858
Yosandi Miguel Reyes <elyosandireyes@gmail.com>
2024-06-19 16:52:02 -0700

    Programa completado

--- assets/js/index/indexJquery.js
+++ assets/js/index/indexJquery.js
@@ -8,22 +8,164 @@ $(document).ready(function () {
 
 
   //events
-  $("#btn-save").on("click", function () {
+  $("#content-container").on("click","#btn-save", function () {
     createContactType();
   });
 
-  $("#btn-clear").on("click", function () { clear() });
+  $("#content-container").on("click","#btn-clear", function () { clear() });
+
+  $("#content-container").on("click","#btn-back",function()
+  {
+    generateHtmlForm();
+  })
+
+  $("#content-container").on("click","#btn-end",function()
+  {
+    const mainContainer = $(this).parent().parent().parent();
+
+    $.confirm(
+      {
+        title:"Esta seguro que desea crear este contacto",
+        content:"",
+        buttons:
+        {
+          cancel:
+          {
+            text: "Cancelar",
+            btnClass: "btn btn-success",
+            action: function(){},
+          },
+          confirm:
+          {
+            text: "Confirmar",
+            btnClass: "btn btn-success",
+            action: function()
+            {
+              generateHtmlContact();
+              generateHtmlForm();
+              clear();
+              toastr.success('Se ha creado con exito.','Notificacion',
+                {
+                  timeOut : 1500,
+                })
+
+            },
+          },
+
+        },
+
+      })
+  })
 
   $("#contact-container").on("click", ".btn-delete", function () {
 
-    if (confirm("Esta seguro que desea aliminar estes contacto")) {
-      const mainContainer = $(this).parent().parent().parent();
-      mainContainer.remove();
-    }
-  })
+    const mainContainer = $(this).parent().parent().parent();
+
+    $.confirm(
+      {
+        title:"Esta seguro que desea aliminar este contacto",
+        content:"",
+        buttons:
+        {
+          cancel:
+          {
+            text: "Cancelar",
+            btnClass: "btn btn-danger",
+            action: function(){},
+          },
+          confirm:
+          {
+            text: "Confirmar",
+            btnClass: "btn btn-success",
+            action: function()
+            {
+              mainContainer.remove();
+            },
+          },
+
+        },
+
+      })
+  });
 
 
   //Funtions
+
+  function generateHtmlForm() {
+    const htmlForm =
+      `        <div class="card">
+          <div class="card-header text-white bg-dark">
+            <h4 class="text-center">Registre su contactos</h4>
+          </div>
+          <div class="card-body">
+            <div class="card-title">
+              <h4 class="text-center">Complete toda la informacion</h4>
+            </div>
+
+            <div class="mb-3">
+              <label for="name" class="form-label">Nombre:</label>
+              <input type="text" class="form-control" id="name" />
+            </div>
+            <div class="mb-3">
+              <label for="phone" class="form-label">Telefono:</label>
+              <input type="text" class="form-control" id="phone" />
+            </div>
+            <div class="mb-3" id="radio-container">
+              <label for="contact-type" class="form-label">Tipo de contacto:</label>
+
+              <div class="form-check form-check-inline">
+                <input clas="form-check-input" type="radio" name="contactType" id="Academico-radio" value="Academico">
+                <label for="Academico-radio" class="form-check-label">Academico</label>
+              </div>
+
+              
+              <div class="form-check form-check-inline">
+                <input clas="form-check-input" type="radio" name="contactType" id="work-radio" value="Laboral">
+                <label for="Academico-radio" class="form-check-label">Laboral</label>
+              </div>
+
+              
+              <div class="form-check form-check-inline">
+                <input clas="form-check-input" type="radio" name="contactType" id="personal-radio" value="Personal">
+                <label for="Academico-radio" class="form-check-label">Personal</label>
+              </div>
+
+            </div>
+            <button type="button" class="btn btn-outline-primary float-end" id="btn-save">
+              Guardar
+            </button>
+            <button type="button" class="btn btn-outline-warning float-end mr-1" id="btn-clear">
+              Limpiar
+            </button>
+          </div>
+        </div>`;
+    $("#content-container").html(htmlForm);
+
+    $("#name").val(ValueName);
+    $("#phone").val(ValuePhone);
+    $(`#radio-container input[type='radio'][value = ${ValueContactType}]`).prop("checked",true);
+
+  }
+
+  function generateHtmlConfirm() {
+    const htmlConfirmation =
+      `    <div class="card">
+            <div class="card-header bg-success text-white">
+              <h5 class="card-title">Confirmacion</h5>
+            </div>
+
+            <div class="card-body">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Tipo de contacto: ${ValueContactType}</li>
+                <li class="list-group-item">Nombre: ${ValueName}</li>
+                <li class="list-group-item">Telefono: ${ValuePhone}</li>
+              </ul>
+              <button id= "btn-end" class="btn btn-success float-end">Finalizar</button>
+              <button id = "btn-back" class="btn btn-danger float-end me-1">Atras</button>
+            </div>
+          </div>`;
+    $("#content-container").html(htmlConfirmation);
+  }
 
   function generateHtmlContact() {
     const htmlContact =
@@ -51,6 +193,9 @@ $(document).ready(function () {
     $("#radio-container input[type= 'radio']:checked").prop("checked", false);
     $("#radio-container").removeClass("input-error");
     $("#radio-container").removeClass("input-success");
+    ValueName = "";
+    ValuePhone = "";
+    ValueContactType = "";
   }
 
   function validation() {
@@ -101,12 +246,7 @@ $(document).ready(function () {
     ValueContactType = $("#radio-container input[type= 'radio']:checked").val();
 
     if (validation()) {
-      generateHtmlContact();
-      clear();
-      toastr.success('El contacto ha sido agregado correctamente','Contacto agregado',
-        {
-          timeOut : 1500,
-        })
+      generateHtmlConfirm();
     } else {
       toastr.error('Favor completar toda la informacion.','Ooop!! ha ocurrido un error',
         {
